3cb0cc5edb36498898a77d4a635d13670f3ceebb
NicolasLLans <nicolaslautaro22@gmail.com>
2024-10-08 20:11:22 -0300

    diseño del dashboard: faltan mejoras

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
