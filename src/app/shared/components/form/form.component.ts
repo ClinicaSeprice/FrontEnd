586078f4d5747f7114a229daf123a4d017f06d64
NicolasLLans <nicolaslautaro22@gmail.com>
2024-11-02 15:31:29 -0300

    Configurarción final de formulario reutilizable y personalizable, prueba de tabla y form en pagina de citas

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,75 +1,85 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass, NgFor, NgIf, TitleCasePipe } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule], 
+  imports: [ReactiveFormsModule, NgClass, NgIf, NgFor, TitleCasePipe],
   template: `
     <form [formGroup]="form" (ngSubmit)="onSubmit()">
-      <div class="grid gap-6 mb-6 md:grid-cols-2">
-        <!-- First Name -->
-        <div>
-          <label [for]="firstNameId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ firstNameLabel }}</label>
-          <input [id]="firstNameId" type="text" formControlName="firstName" [placeholder]="firstNamePlaceholder" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        
-        <!-- Last Name -->
-        <div>
-          <label [for]="lastNameId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ lastNameLabel }}</label>
-          <input [id]="lastNameId" type="text" formControlName="lastName" [placeholder]="lastNamePlaceholder" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        
-        <!-- Email -->
-        <div>
-          <label [for]="emailId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ emailLabel }}</label>
-          <input [id]="emailId" type="email" formControlName="email" [placeholder]="emailPlaceholder" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        
-        <!-- Password -->
-        <div>
-          <label [for]="passwordId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ passwordLabel }}</label>
-          <input [id]="passwordId" type="password" formControlName="password" [placeholder]="passwordPlaceholder" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+      <div *ngFor="let field of formConfig" class="mb-6">
+        <!-- Etiqueta del campo -->
+        <label
+          [for]="field.name"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >{{ field.name | titlecase }}</label
+        >
+
+        <!-- Campo de entrada -->
+        <input
+          [id]="field.name"
+          [type]="field.type"
+          [formControlName]="field.name"
+          [placeholder]="field.placeholder"
+          [ngClass]="{
+            'border-gray-300':
+              !form.controls[field.name].invalid ||
+              form.controls[field.name].pristine,
+            'border-red-500':
+              form.controls[field.name].invalid &&
+              form.controls[field.name].touched,
+          }"
+          class="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+        <!-- Mensaje de error -->
+        <div
+          *ngIf="
+            form.controls[field.name].invalid &&
+            form.controls[field.name].touched
+          "
+          class="text-red-500 text-sm">
+          {{ field.errorMessage || (field.name | titlecase) + ' es requerido.' }}
         </div>
       </div>
 
-      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      <!-- Botón de submit -->
+      <button type="submit" [ngClass]="submitButtonClasses">
+        {{ submitButtonText }}
+      </button>
     </form>
   `,
-  styles: [``],
 })
-export class FormComponent {
-  @Input() firstNameLabel = 'First Name';
-  @Input() firstNamePlaceholder = 'John';
-  @Input() firstNameId = 'first_name';
+export class FormComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  @Input() submitButtonText = 'Enviar';
+  @Input() submitButtonClasses =
+    'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800';
 
-  @Input() lastNameLabel = 'Last Name';
-  @Input() lastNamePlaceholder = 'Doe';
-  @Input() lastNameId = 'last_name';
+  @Input() formConfig: {
+    name: string;
+    type: string;
+    placeholder: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    validators: any[];
+    errorMessage?: string; // Mensaje de error personalizado
+  }[] = [];
+  @Output() formSubmit = new EventEmitter<object>(); // Emisor de evento para enviar los datos
 
-  @Input() emailLabel = 'Email Address';
-  @Input() emailPlaceholder = 'john.doe@company.com';
-  @Input() emailId = 'email';
+  form: FormGroup = {} as FormGroup;
 
-  @Input() passwordLabel = 'Password';
-  @Input() passwordPlaceholder = '•••••••••';
-  @Input() passwordId = 'password';
-  @Output() formSubmit = new EventEmitter<object>(); // Emisor de evento para enviar datos
-  form: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+  ngOnInit(): void {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const formGroupConfig: any = {};
+    this.formConfig.forEach(field => {
+      formGroupConfig[field.name] = ['', field.validators || []];
     });
+    this.form = this.fb.group(formGroupConfig);
   }
 
   onSubmit(): void {
     if (this.form.valid) {
-      console.log('Form Submitted', this.form.value);
-      this.formSubmit.emit(this.form.value); // Emitir los datos del formulario
+      this.formSubmit.emit(this.form.value); // Emitir los datos del formulario al componente padre
     }
   }
 }
