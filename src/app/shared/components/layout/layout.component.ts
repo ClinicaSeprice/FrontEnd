0cefb45e8992213d8c289a035e5d66e95390dd6f
NicolasLLans <nicolaslautaro22@gmail.com>
2024-11-02 00:59:51 -0300

    se configura el sidebar para que sea responsive y se active la vista desde el menu hamburguesa del navbar y se pueda cerrar en dispositivos moviles

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../sidebar/sidebar.component";
@@ -12,5 +12,10 @@ import { NavbarComponent } from "../navbar/navbar.component";
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  isSidebarVisible = signal(false);
+
+  toggleSidebar() {
+      this.isSidebarVisible.update((SidebarVisible) => !SidebarVisible);
+  }
 
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive, NgClass],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  @Input() isVisible = true;
+  @Output() closeSidebar = new EventEmitter<void>();
 }
